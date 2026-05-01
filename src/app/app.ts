a595107d50ea29917058a2236fde3944a1450366
Maina-git <francismm2023@gmail.com>
2026-05-01 10:26:49 +0300

    added angular components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
+import { About } from './about/about';
+import { Contacts } from './contacts/contacts';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Profile, About, Contacts],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('MYANGULAR');
 }
+
+
+
+
+
+
+
+
+
+
